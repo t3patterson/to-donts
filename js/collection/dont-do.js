2bb47ee01a9702ca0b90593e4e0b74aecb8427b7
travis <t3patterson@gmail.com>
2016-04-01 09:00:25 -0500

    connecting to no-auth endpoint

--- js/collection/dont-do.js
+++ js/collection/dont-do.js
@@ -3,12 +3,12 @@ import BackboneFire from 'bbfire'
 export var DontDoModel = BackboneFire.Firebase.Model.extend({
   url: function(id){
     if (!id) id = 'dont-do.js'
-    return "https://todo4u.firebaseio.com/"+id
+    return "https://todo4u.firebaseio.com/noauth/"+id
   }
 })
 
 export var DontDoCollection = BackboneFire.Firebase.Collection.extend({
   autoSync: true,
-  url: "https://todo4u.firebaseio.com/"
+  url: "https://todo4u.firebaseio.com/noauth/"
 })
 
